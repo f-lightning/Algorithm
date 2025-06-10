02ea48159762eaa9f835ac3c8226655bb057c0b3
seungyeon-lee <lee6034770@gmail.com>
2025-06-10 09:21:10 +0900

    (10171) 고양이

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|";
+        System.out.println(str);
+    }
+}
